b2b56f840b8c3560f693dff859f92be121f27e89
Stratis Dermanoutsos <stratis450@gmail.com>
2023-08-31 22:32:26 +0300

    Home styling prep

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,26 @@
+import Color from '@/Colors';
+import useDarkMode from '@/hooks/useDarkMode';
 import {
+    StyleSheet,
     Text,
     View
 } from 'react-native';
 
-const Home = (props: any) => {
+const Home = (props: any): JSX.Element => {
+    const isDarkMode = useDarkMode();
+
+    const styles = StyleSheet.create({
+        background: {
+            backgroundColor: isDarkMode ? Color.BACKGROUND_DARKEST : Color.BACKGROUND_LIGHT,
+            display: 'flex',
+            flexDirection: 'row',
+            padding: 16,
+            minHeight: '100%'
+        }
+    });
+
     return (
-        <View>
+        <View style={styles.background}>
             <Text>This is home view</Text>
         </View>
     );
