e2c59161a02a136373da99b68c4579b8da48b6db
Stratis Dermanoutsos <stratis450@gmail.com>
2023-09-02 13:08:36 +0300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -9,15 +9,15 @@ import route from '@/types/route';
 import Absences from '@/screens/Absences';
 import Home from '@/screens/Home';
 import Sync from '@/screens/Sync';
-import appSettings from './types/app-settings';
+import appSettings from '@/types/app-settings';
 import listItem from '@/types/list-item';
+import dbInfo from '@/types/db-info';
 
 export const API_URL: string = 'http://localhost:3000/api';
 
-// TODO: Load from API (They should be set in the Admin webapp)
-export const settings: appSettings = {
-    schoolName: '5ο ΓΕΛ Βύρωνος',
-    schoolLogoUrl: 'https://5lykeiovyrona.gr/img/logo-mobile.png'
+export const DB_INFO: dbInfo = {
+    fileName: 'students-absence.db',
+    location: 'default'
 };
 
 export const ROUTES: route[] = [{
@@ -33,6 +33,12 @@ export const ROUTES: route[] = [{
     labelKey: 'LABEL_SYNC',
     screen: Sync
 }];
+
+// TODO: Load from API (They should be set in the Admin webapp)
+export const settings: appSettings = {
+    schoolName: '5ο ΓΕΛ Βύρωνος',
+    schoolLogoUrl: 'https://5lykeiovyrona.gr/img/logo-mobile.png'
+};
 
 export const teachers: listItem[] = [{
     id: 1,
