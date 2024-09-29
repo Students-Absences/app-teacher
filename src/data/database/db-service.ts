43bb3815581f8cc6e7c27f227ab366b19bf5033c
Stratis Dermanoutsos <stratis450@gmail.com>
2024-09-29 14:31:56 +0300

    Update db-service.ts

--- src/data/database/db-service.ts
+++ src/data/database/db-service.ts
@@ -37,7 +37,7 @@ export const initializeDb = async (): Promise<void> => {
     try {
         const db = await getDbConnection();
 
-        // dropAllTables(db);
+//         dropAllTables(db);
         await createAllTables(db);
     } catch (error) {
         console.error(error);
